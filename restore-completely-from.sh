603ee1325107587ddb63e78916b0dc559a8a8b7f
Waog <hello@waog.net>
2016-05-22 17:33:18 +0200

    FIX relative script paths

--- restore-completely-from.sh
+++ restore-completely-from.sh
@@ -5,7 +5,7 @@ docker stop wordpress
 docker rm -v wordpress
 
 #create and restore new wp container
-./create-rpi-wordpress.sh
-./run-rpi-wordpress-restore-from.sh $1
+SCRIPT_PATH="`dirname \"$0\"`"
+$SCRIPT_PATH/create-rpi-wordpress.sh
+$SCRIPT_PATH/run-rpi-wordpress-restore-from.sh $1
 docker start wordpress
-
